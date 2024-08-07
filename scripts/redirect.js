39f98982e9a469cb8243f3b3e3908aaeb9aad999
Shaga <122209179+fringed-iris@users.noreply.github.com>
2024-08-07 18:52:15 +0900

    リダイレクトの記録方法を変更

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,18 +1,21 @@
 "use strict";
 
-const redirect = (originId, data) => {
-    const ORG = document.getElementById(originId);
-    const C_URL = new URL(location.href);
+const redirect = (orgId, dir) => {
+    const ORG = document.getElementById(orgId);
     const ADR = {
         from: location.pathname.split("/")[2] ?? "",//https://newflorrio.wiki.fc2.com[0]/[1]/[2]/
-        to: data
+        to: dir
     }
 
-    if (!C_URL.searchParams.get("rfrom") && ADR.from && ADR.to) {
+    if (sessionStorage.getItem("redirect") && ADR.from && ADR.to) {
+        sessionStorage.setItem("redirect", ADR.from)
+
         location.replace(`https://newflorrio.wiki.fc2.com/wiki/${ADR.to}?rfrom=${ADR.from}`);
     } else {
+        sessionStorage.setItem("redirect", "");
+
         const P = document.createElement("p");
-        P.textContent = "リダイレクトに失敗しました。";
+        P.textContent = `${ADR.from}から${ADR.to}へのリダイレクトに失敗しました。`;
         ORG.after(P);
 
         console.error("リダイレクトに失敗しました。\n%o\n%o", ADR, ORG);
