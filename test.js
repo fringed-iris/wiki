2ed82127f5d733ede63ae07b190c961071978517
Shaga <122209179+fringed-iris@users.noreply.github.com>
2024-08-12 19:14:58 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
+import settings from "./test.json" assert { type: "json" };
+
 export function test(msg) { console.log(`message from test.js: ${msg}`) }
+export function load(){ console.log(settinggs) }
