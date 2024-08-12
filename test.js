37522044e90de83ada631ebcedcc92f6c15a0787
Shaga <122209179+fringed-iris@users.noreply.github.com>
2024-08-12 19:30:40 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-import settings from "./test.json" assert { type: "json" };
-
 export function test(msg) { console.log(`message from test.js: ${msg}`) }
-export function load(){ console.log(settinggs) }
+export async function load(){
+  const req = await fetch(./test.json);
+  const res = await req.json()
+  console.log(res); 
+}
