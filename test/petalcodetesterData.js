9278efd9d8de31ca9c3a94f1dc6b3b7a4dbbbbd1
hidehidev7 <hidehidev7@gmail.com>
2024-09-07 16:52:52 +0900

    modified:   test/petalcodetester.html new file:   test/petalcodetesterData.js petalcodetesterを整備した

---
+++ test/petalcodetesterData.js
@@ -0,0 +1,19 @@
+export const options = {
+    "name": "Basic",
+    "petalCount": 1,
+    "baseHealth": 10,
+    "baseDamage": 10,
+    "reloadTime": 2.5,
+    "reloadUniqueTimes": [2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5],
+    "calcDPS": false,
+    "leastRarity": 0,
+    "specialStatus": [
+        {
+            width: 150,
+            name: "ドロップスコア",
+            type: "unique",
+            last: "",
+            uniqueDatas: ["ドロップしない", "20~", "175~", "2625~", "72188~", "6196094~", "790001954~", "199475493164~"]
+        },
+    ]
+};
