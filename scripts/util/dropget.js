99329bf8599097bc1e7ccb685c7534bf34f3b464
hidehidev7 <hidehidev7@gmail.com>
2025-03-04 20:02:25 +0900

    getAll編集中

--- scripts/util/dropget.js
+++ scripts/util/dropget.js
@@ -19,10 +19,28 @@ for(let mob = 0; mob < ${window.florr.rarity.length}; mob++) { //mob
 }
 dropTableStr += "]";
 console.log(dropTableStr);`,
-    getDropTableZip: `let t="[";for(let m=0;m<${window.florr.rarity.length};m++){if(m!==0)t+=",";let l="[";for(let p=0;p<${window.florr.rarity.length};p++){if(p!==0)l+=",";let d=florrio.utils.calculateDropChance(${baseChance},m,p).toFixed(8);if(d==0)d="0";l+=d}l+="]";t+=l}t+="]";console.log(t);`,
+        getDropTableZip: `let t="[";for(let m=0;m<${window.florr.rarity.length};m++){if(m!==0)t+=",";let l="[";for(let p=0;p<${window.florr.rarity.length};p++){if(p!==0)l+=",";let d=florrio.utils.calculateDropChance(${baseChance},m,p).toFixed(8);if(d==0)d="0";l+=d}l+="]";t+=l}t+="]";console.log(t);`,
     }
+    main.getAll = `
+const BaseChanceArr = ＜入力＞;
+let wholeStr = "";
+BaseChanceArr.forEach(baseChance => {
+    let dropTableStr = "[";
+    for(let mob = 0; mob < ${window.florr.rarity.length}; mob++) { //mob
+        if(mob !== 0) dropTableStr += ","
+        let dropListStr = "[";
+        for(let petal = 0; petal < ${window.florr.rarity.length}; petal++) { //petal
+            if(petal !== 0) dropListStr += ","
+            let dropStr = florrio.utils.calculateDropChance(baseChance,mob,petal).toFixed(8);
+            if(dropStr == 0) dropStr = "0";
+            dropListStr += dropStr;
+        }
+        dropListStr += "]";
+        dropTableStr += dropListStr;
+    }
+    dropTableStr += "]";
+    wholeStr += "'" + baseChanceArr.toString() + "': " + dropTableStr + ",";
+});
+console.log(wholeStr);`
     return main;
 }
-
-//expected function
-
